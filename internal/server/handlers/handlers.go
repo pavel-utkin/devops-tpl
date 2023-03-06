0ed7fba71ef9c1d4869b3c59ddf42eb81f032e47
Pavel Utkin <pavel.utkin@abbyy.com>
2023-03-07 02:38:00 +0300

    INC 2

---
+++ internal/server/handlers/handlers.go
@@ -0,0 +1,83 @@
+package handlers
+
+import (
+	"devops-tpl/internal/server/storage"
+	"fmt"
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func UpdateGaugePost(rw http.ResponseWriter, request *http.Request, memStatsStorage storage.MemStatsMemoryRepo) {
+	statName := chi.URLParam(request, "statName")
+	statValue := chi.URLParam(request, "statValue")
+	statValueInt, err := strconv.ParseFloat(statValue, 64)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("Bad request"))
+		return
+	}
+
+	err = memStatsStorage.UpdateGaugeValue(statName, statValueInt)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte("Server error"))
+		return
+	}
+
+	fmt.Println("Update gauge:")
+	fmt.Printf("%v: %v\n\n", statName, statValue)
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("Ok"))
+}
+
+func UpdateCounterPost(rw http.ResponseWriter, request *http.Request, memStatsStorage storage.MemStatsMemoryRepo) {
+	statName := chi.URLParam(request, "statName")
+	statValue := chi.URLParam(request, "statValue")
+	statCounterValue, err := strconv.ParseInt(statValue, 10, 64)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+
+	err = memStatsStorage.UpdateCounterValue(statName, statCounterValue)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+
+	log.Println("Inc counter:")
+	log.Printf("%v: %v\n", statName, statValue)
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("Ok"))
+}
+
+func UpdateNotImplementedPost(rw http.ResponseWriter, request *http.Request) {
+	log.Println("Update not implemented statType")
+	rw.WriteHeader(http.StatusNotImplemented)
+	rw.Write([]byte("Not implemented"))
+}
+
+func PrintStatsValues(rw http.ResponseWriter, request *http.Request, memStatsStorage storage.MemStatsMemoryRepo) {
+	htmlTemplate := `
+		<html>
+			<head>
+			<title></title>
+			</head>
+			<body>
+				%v
+			</body>
+		</html>`
+	keyValuesHTML := ""
+
+	for k, v := range memStatsStorage.GetDBSchema() {
+		keyValuesHTML += fmt.Sprintf("<div><b>%v</b>: %v</div>", k, v)
+	}
+
+	htmlPage := fmt.Sprintf(htmlTemplate, keyValuesHTML)
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(htmlPage))
+}
