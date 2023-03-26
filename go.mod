6de1af6dd4e849a5473d6f696020adf9144604f3
Pavel Utkin <pavel.utkin@abbyy.com>
2023-03-26 17:46:32 +0300

    iter4

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.19
 require (
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/stretchr/testify v1.7.1
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- internal/agent/requesthandler/requesthandler.go
+++ internal/agent/requesthandler/requesthandler.go
@@ -3,6 +3,7 @@ package requesthandler
 import (
 	"devops-tpl/internal/agent/config"
 	"devops-tpl/internal/agent/statsreader"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/go-resty/resty/v2"
@@ -30,6 +31,58 @@ func oneStatUpload(httpClient *resty.Client, statType, statName, statValue strin
 	}
 	if resp.StatusCode() != http.StatusOK {
 		return errors.New("HTTP Status != 200")
+	}
+
+	return nil
+}
+
+func oneStatUploadJSON(httpClient *resty.Client, statType string, statName string, statValue string) error {
+	OneMetrics := struct {
+		ID    string  `json:"id"`
+		MType string  `json:"type"`
+		Delta int64   `json:"delta"`
+		Value float64 `json:"value"`
+	}{
+		ID:    statName,
+		MType: statType,
+	}
+
+	var err error
+	switch OneMetrics.MType {
+	case "counter":
+		var metricValue int64
+		metricValue, err = strconv.ParseInt(statValue, 10, 64)
+		OneMetrics.Delta = metricValue
+	case "gauge":
+		var metricValue float64
+		metricValue, err = strconv.ParseFloat(statValue, 64)
+		OneMetrics.Value = metricValue
+	default:
+		return errors.New("unknow statType")
+	}
+	if err != nil {
+		return errors.New("invalid statValue")
+	}
+
+	statJSON, err := json.Marshal(OneMetrics)
+	if err != nil {
+		return err
+	}
+
+	resp, err := httpClient.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(string(statJSON)).
+		SetPathParams(map[string]string{
+			"host": config.ServerHost,
+			"port": strconv.Itoa(config.ServerPort),
+		}).
+		Post("http://{host}:{port}/update/")
+
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode() != 200 {
+		return fmt.Errorf("HTTP Status: %v (not 200)", resp.StatusCode())
 	}
 
 	return nil
