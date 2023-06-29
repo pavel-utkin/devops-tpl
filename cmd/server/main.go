36c7cd05b1eb24ef2df5cba2012e530e1e1a9b93
Pavel Utkin <pavel.utkin@abbyy.com>
2023-06-30 00:03:52 +0300

    iter19

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,10 @@ import (
 	"os"
 )
 
+var buildVersion = "N/A"
+var buildDate = "N/A"
+var buildCommit = "N/A"
+
 func Profiling(addr string) {
 	err := http.ListenAndServe(addr, nil)
 	if errors.Is(err, http.ErrServerClosed) {
@@ -23,6 +27,11 @@ func Profiling(addr string) {
 }
 
 func main() {
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	config := config.LoadConfig()
 	server := server.NewServer(config)
 
