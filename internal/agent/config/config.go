aa6dfa033d6fa8dc370a60d7be91728eaa063567
Pavel Utkin <pavel.utkin@abbyy.com>
2023-07-21 08:11:47 +0300

    iter21

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
+	"os"
 	"time"
 
 	"github.com/caarlos0/env/v6"
@@ -11,31 +13,31 @@ import (
 // HTTPClientConfig используется для хранения конфигурации агента, связанной с настройкой http клиента.
 type HTTPClientConfig struct {
 	// RetryCount - количество попыток отправки запроса (default: 2)
-	RetryCount int `env:"RETRY_CONN_COUNT"`
+	RetryCount int `env:"RETRY_CONN_COUNT" json:"retry_count,omitempty"`
 	// RetryWaitTime - время ожидания между попытками отправки запроса (default: 10s)
-	RetryWaitTime time.Duration `env:"RETRY_CONN_WAIT_TIME"`
+	RetryWaitTime time.Duration `env:"RETRY_CONN_WAIT_TIME" json:"retry_wait_time,omitempty"`
 	// RetryMaxWaitTime - макс. время для попыток отправки запроса (default: 90s)
-	RetryMaxWaitTime time.Duration `env:"RETRY_CONN_MAX_WAIT_TIME"`
+	RetryMaxWaitTime time.Duration `env:"RETRY_CONN_MAX_WAIT_TIME" json:"retry_max_wait_time,omitempty"`
 	// ServerAddr - адрес сервера (default: 127.0.0.1:8080)
-	ServerAddr string `env:"ADDRESS"`
+	ServerAddr string `env:"ADDRESS" json:"address,omitempty"`
 }
 
 // Config используется для хранения конфигурации агента.
 type Config struct {
 	// PollInterval - интервал между считыванием метрик (flag: p; default: 2s)
-	PollInterval time.Duration `env:"POLL_INTERVAL"`
+	PollInterval time.Duration `env:"POLL_INTERVAL" json:"poll_interval,omitempty"`
 	// ReportInterval - интервал между отправки метрик (flag: r; default: 2s)
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" json:"report_interval,omitempty"`
 	// PublicKeyRSA - публичный RSA ключ (flag: crypto-key)
-	PublicKeyRSA string `env:"CRYPTO_KEY"`
+	PublicKeyRSA string `env:"CRYPTO_KEY" json:"crypto_key,omitempty"`
 	// SignKey - ключ для подписи сообщений (flag: k)
-	SignKey string `env:"KEY"`
+	SignKey string `env:"KEY" json:"sign_key,omitempty"`
 	// RateLimit
-	RateLimit int `env:"RATE_LIMIT"`
+	RateLimit int `env:"RATE_LIMIT" json:"rate_limit,omitempty"`
 	// LogFile - лог файл (flag: l)
-	LogFile string `env:"LOG_FILE"`
+	LogFile string `env:"LOG_FILE" json:"log_file,omitempty"`
 	// DebugMode - debug мод (flag: d)
-	DebugMode            bool `env:"DEBUG"`
+	DebugMode            bool `env:"DEBUG"  json:"debug,omitempty"`
 	HTTPClientConnection HTTPClientConfig
 }
 
@@ -59,6 +61,39 @@ func newConfig() *Config {
 	return &config
 }
 
+func (config *Config) parseConfig(flagConfigPath, flagConfigPathAlias *string) {
+	var configPath string
+	if *flagConfigPath != "" {
+		configPath = *flagConfigPath
+	}
+
+	if *flagConfigPathAlias != "" {
+		configPath = *flagConfigPathAlias
+	}
+
+	if path, ok := os.LookupEnv("CONFIG"); ok {
+		configPath = path
+	}
+
+	if configPath == "" {
+		return
+	}
+
+	file, err := os.OpenFile(configPath, os.O_RDONLY|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func (config *Config) parseEnv() error {
 	return env.Parse(config)
 }
@@ -70,14 +105,18 @@ func (config *Config) parseFlags() {
 	flag.StringVar(&config.HTTPClientConnection.ServerAddr, "a", config.HTTPClientConnection.ServerAddr, "server address (host:port)")
 	flag.StringVar(&config.SignKey, "k", config.SignKey, "sign key")
 	flag.IntVar(&config.RateLimit, "l", config.RateLimit, "number of concurrent requests to the server")
-	flag.BoolVar(&config.DebugMode, "d", config.DebugMode, "debug mode \"\"")
+	flag.BoolVar(&config.DebugMode, "d", config.DebugMode, "debug mode")
 	flag.Parse()
 }
 
 func LoadConfig() Config {
 	config := newConfig()
 
+	flagConfigPath := flag.String("c", "", "path to json config")
+	flagConfigPathAlias := flag.String("config", "", "path to json config")
+
 	config.parseFlags()
+	config.parseConfig(flagConfigPath, flagConfigPathAlias)
 	err := config.parseEnv()
 
 	if config.RateLimit == 0 {
